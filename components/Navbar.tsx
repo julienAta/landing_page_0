02e14d6cc1f70b8dc9d686ba190d1d6a05ed6e62
jjwebsolutions <jj.websolutions@proton.me>
2023-10-10 19:11:53 +0200

    css

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import * as React from "react";
+
+export function Navbar() {
+  return (
+    <>
+      <div className="container flex  items-center mx-auto mt-5">Navbar</div>
+    </>
+  );
+}
